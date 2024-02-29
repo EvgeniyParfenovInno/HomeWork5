5bd120fcfe73b897109ea5cea1a085e3f66c8c8a
Парфенов Евгений <esparfenov@vtb.ru>
2024-02-29 08:55:48 +0300

    Рефакторинг

--- src/main/java/ru/demo/homework/config/DataSourceConfig.java
+++ src/main/java/ru/demo/homework/config/DataSourceConfig.java
@@ -2,47 +2,19 @@ package ru.demo.homework.config;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.sql.DataSource;
 
-@Configuration
-public class DataSourceConfig {
-
-    @Value("${spring.datasource.hikari.jdbc-url}")
-    private String url;
-
-    @Value("${spring.datasource.hikari.driver-class-name}")
-    private String driverName;
-
-    @Value("${spring.datasource.hikari.username}")
-    private String username;
-
-    @Value("${spring.datasource.hikari.password}")
-    private String password;
-
-    @Bean
-    public DataSource hikariDataSource() {
-        HikariConfig config = new HikariConfig();
-        config.setJdbcUrl(url);
-        config.setDriverClassName(driverName);
-        config.setUsername(username);
-        config.setPassword(password);
-        return new HikariDataSource(config);
-    }
-}
-/*
 @EnableAutoConfiguration
 @Configuration
-@ConfigurationProperties("spring.datasource")
+@ConfigurationProperties("spring.datasource.hikari")
 public class DataSourceConfig extends HikariConfig {
     @Bean
     public DataSource hikariDataSource() {
-        //HikariConfig config = new HikariConfig("/hikaricp.properties");
-        System.out.println(this);
         return new HikariDataSource(this);
     }
 }
-*/
